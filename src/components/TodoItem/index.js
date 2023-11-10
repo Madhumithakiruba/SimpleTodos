e1d5c9f6869dbbaa3de4a462f37e2d2b7d55c50c
Madhu_mitha <madhukiruba99@gmail.com>
2023-11-10 07:53:07 +0000

    Simple Todos

---
+++ src/components/TodoItem/index.js
@@ -0,0 +1,20 @@
+// Write your code here
+
+import './index.css'
+
+const TodoItem = props => {
+  const {todos, deleteItem} = props
+  const {id, title} = todos
+  const deleteElement = () => {
+    deleteItem(id)
+  }
+  return (
+    <li className="list-styling">
+      <p className="paragraph-style">{title}</p>
+      <button type="button" className="button-style" onClick={deleteElement}>
+        Delete
+      </button>
+    </li>
+  )
+}
+export default TodoItem
